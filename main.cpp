bf3ad6d9575ca5b69e2d740421c9866a59e81f49
itsamal0 <amal@itsamal.com>
2025-12-21 11:34:16 +0200

    feat: implement ClsCurrency and test its functionality

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
-#include "ClsLoginScreen.h"
+#include "ClsCurrency.h"
+using namespace std;
+
+void printCurrencyCard(ClsCurrency currency) {
+    cout << "\nCurrency Card:\n";
+    cout << "_____________________________\n";
+    cout << "\nCountry    : " << currency.country();
+    cout << "\nCode       : " << currency.currencyCode();
+    cout << "\nName       : " << currency.currencyName();
+    cout << "\nRate(1$) = : " << currency.rate();
+    cout << "\n_____________________________\n";
+}
 
 int main() {
-    
-    while (true) {
+    ClsCurrency currency1 = ClsCurrency::findByCode("jod");
 
-        if (!ClsLoginScreen::showLoginScreen()) 
-            break;
+    if (currency1.isEmpty()) {
+        cout << "\nCurrency Is Not Found!\n";
+    } 
+    else {
+        printCurrencyCard(currency1);
     }
-    
+
+    ClsCurrency currency2 = ClsCurrency::findByCountry("Egypt");
+
+    if (currency2.isEmpty()) {
+        cout << "\nCurrency Is Not Found!\n";
+    } 
+    else {
+        printCurrencyCard(currency2);
+    }
+
+    cout << "\nCurrency1 after updating Rate:\n";
+    currency1.updateRate(0.75);
+    printCurrencyCard(currency1);
+
     system("pause>0");
     return 0;
 }
